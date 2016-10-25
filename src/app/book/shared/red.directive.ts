68887491638a2769c54f334f377ffe54bedd8363
Bengt Weiße <bengtler@gmail.com>
2016-10-25 15:37:22 +0200

    fill directive with functionality

--- src/app/book/shared/red.directive.ts
+++ src/app/book/shared/red.directive.ts
@@ -1,10 +1,15 @@
-import { Directive } from '@angular/core';
+import { Directive, ElementRef, HostListener } from '@angular/core';
 
 @Directive({
   selector: '[bmRed]'
 })
 export class RedDirective {
 
-  constructor() { }
+  constructor(private elementRef: ElementRef) { }
+
+  @HostListener('click')
+  toggleRed() {
+    this.elementRef.nativeElement.style.backgroundColor = 'red';
+  }
 
 }
